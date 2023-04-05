a1c08b8dc8e9c5c2f3a14ef37c37f9e3229cb5f2
benevolent0505 <mikio.fjt@gmail.com>
2023-04-06 05:25:47 +0900

    add test

---
+++ sentrytracing/tracer_test.go
@@ -0,0 +1,44 @@
+package sentrytracing_test
+
+import (
+	"encoding/json"
+	"net/http"
+	"net/http/httptest"
+	"strings"
+	"testing"
+
+	"github.com/99designs/gqlgen/graphql/handler/testserver"
+	"github.com/99designs/gqlgen/graphql/handler/transport"
+	"github.com/benevolent0505/gqlgen-tracing-sentry/sentrytracing"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"github.com/vektah/gqlparser/v2/gqlerror"
+)
+
+func Test_NoError(t *testing.T) {
+	mux := http.NewServeMux()
+
+	srv := testserver.New()
+	srv.AddTransport(transport.POST{})
+	srv.Use(sentrytracing.Tracer{})
+
+	mux.Handle("/query", srv)
+
+	resp := doRequest(mux, http.MethodPost, "/query", `{ "query": "{ name find(id: 1) }"}`)
+	assert.Equal(t, http.StatusOK, resp.Code, resp.Body.String())
+
+	var respData struct {
+		Errors gqlerror.List
+	}
+	require.NoError(t, json.Unmarshal(resp.Body.Bytes(), &respData))
+
+}
+
+func doRequest(handler http.Handler, method, target, body string) *httptest.ResponseRecorder {
+	r := httptest.NewRequest(method, target, strings.NewReader(body))
+	r.Header.Set("Content-Type", "application/json")
+	w := httptest.NewRecorder()
+
+	handler.ServeHTTP(w, r)
+	return w
+}
